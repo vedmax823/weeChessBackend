f9034512117b4ab724b31e7ccf5ed2132f35d848
vedmax823 <107627040+vedmax823@users.noreply.github.com>
2024-07-28 23:08:33 +0100

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,24 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import jest from 'eslint-plugin-jest'
 
 
 export default [
+  {
+    ignores: ['dist']
+  },
+
   {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.node }},
+  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    files : ["*.test.{js, ts, tsx, jsx"],
+    ...jest.configs["flat/recommended"],
+    rules : {
+      ...jest.configs["flat/recommended"].rules,
+      "jest/prefer-expect-assertions" : "off",
+    }
+  }
 ];
